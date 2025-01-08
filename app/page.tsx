b14b8b4ec382c150fe4991c2a1b4a9546a023a24
Dale Hutchinson <daletristanhutchinson@gmail.com>
2025-01-08 16:02:05 +1100

    Basic layout and styling

--- app/page.tsx
+++ app/page.tsx
@@ -2,100 +2,43 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <main className="w-2/3 flex flex-col justify-start items-start content-between gap-5">
+      <h1 className="font-bold text-[2.5rem]">YourSite</h1>
+      <hr className="border-[#fafafa] w-full border-[1.5px]" />
+      <div className="grid grid-cols-2 gap-x-2">
+        <div className="flex flex-col gap-4">
+          <img src="/placeholder-img.png" width={432} height={225} />
+          <p className="text-base">Hi, I'm &lt;your-name&gt;. I'm responsible for:</p>
+          <ul className="  ">
+            <li><a href="#">link 1</a>, my video channel.</li>
+            <li><a href="#">link 2</a>, my open-source text guide platform.</li>
+            <li><a href="#">link 4</a>, my band, also found on <a href="https://bandcamp.org">Bandcamp.</a></li>
+          </ul>
+          <p>Regardless of how you found me, welcome. This is my personal website, home to my <a href="#">blog</a>.</p>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        <div className="flex flex-col justify-start items-start gap-5 text-base">
+          <h2 className="text-2xl">Blog</h2>
+          <ul>
+            <li>24/12/24 - The Downfall of Alternative Frontends</li>
+            <li>17/11/24 - <a>Why I Wrote Galaxy Garner</a></li>
+            <li>20/01/24 - <a>18 Rules Over Technology</a></li>
+            <li>19/11/23 - <a>Are You Still With Me?</a></li>
+            <li>10/09/23 - <a>Every Chat Client Absolutely Sucks.</a></li>
+            <li>01/07/23 - <a>Say No to Self-Deprecating Humor</a></li>
+            <li>05/06/23 - <a>EasyPeasy Way to Quit Pornography</a></li>
+            <li><a><i>More...</i></a></li>
+          </ul>
+          <h2>Donate</h2>
+          <p>Whether you’ve learned something new from my videos, enjoyed my music or have strong opinions about my articles, you can support what I do by donating <a>Monero</a>.</p>
+        <div className="h-11 overflow-hidden flex flex-row flex-nowrap w-full border-[1px] p-1 border-[#fafafa] border-solid items-center"><p className="text-[1.2rem] font-mono ">48dnPpGgo8WernVJp5VhvhaX3u9e46NujdYA44u8zuMdETNC5jXiA9S7JoYMM6qRt1ZcKpt1J3RZ3JPuMyXetmbHH7Mnc9C</p></div>
+        </div>
+      </div>
+      <hr className="border-[#fafafa] w-full border-[1.5px]" />
+      <p className="self-center">[<a>email</a>] ~ [<a>i2p</a>] ~ [<a>tor</a>] ~ [<a>xmpp</a>] </p>
+      <div className="flex flex-row self-center justify-center items-center gap-2">
+        <img src="/banporn.png" width={88} height={31} />
+        <a className="!no-underline"><div className="border-double border-2 border-[#3f83d5] w-[88px] h-[31px] flex items-center justify-center"><p className="font-serif font-black text-[1.4rem] text-center">Denshi</p></div></a>
+      </div>
+    </main>
   );
 }
