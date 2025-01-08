3c9cb70d54ab7670a09179e6ccc55c487beb9403
Dale Hutchinson <daletristanhutchinson@gmail.com>
2025-01-08 16:08:32 +1100

    Update homepage layout for improved responsiveness and styling

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="w-2/3 flex flex-col justify-start items-start content-between gap-5">
+    <main className="w-11/12 md:w-2/3 flex flex-col justify-start items-start content-between gap-5">
       <h1 className="font-bold text-[2.5rem]">YourSite</h1>
       <hr className="border-[#fafafa] w-full border-[1.5px]" />
-      <div className="grid grid-cols-2 gap-x-2">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-2">
         <div className="flex flex-col gap-4">
           <img src="/placeholder-img.png" width={432} height={225} />
           <p className="text-base">Hi, I'm &lt;your-name&gt;. I'm responsible for:</p>
@@ -17,7 +17,7 @@ export default function Home() {
           <p>Regardless of how you found me, welcome. This is my personal website, home to my <a href="#">blog</a>.</p>
         </div>
         <div className="flex flex-col justify-start items-start gap-5 text-base">
-          <h2 className="text-2xl">Blog</h2>
+          <h2 className="text-2xl font-bold">Blog</h2>
           <ul>
             <li>24/12/24 - The Downfall of Alternative Frontends</li>
             <li>17/11/24 - <a>Why I Wrote Galaxy Garner</a></li>
@@ -28,7 +28,7 @@ export default function Home() {
             <li>05/06/23 - <a>EasyPeasy Way to Quit Pornography</a></li>
             <li><a><i>More...</i></a></li>
           </ul>
-          <h2>Donate</h2>
+          <h2 className="font-bold">Donate</h2>
           <p>Whether you’ve learned something new from my videos, enjoyed my music or have strong opinions about my articles, you can support what I do by donating <a>Monero</a>.</p>
         <div className="h-11 overflow-hidden flex flex-row flex-nowrap w-full border-[1px] p-1 border-[#fafafa] border-solid items-center"><p className="text-[1.2rem] font-mono ">48dnPpGgo8WernVJp5VhvhaX3u9e46NujdYA44u8zuMdETNC5jXiA9S7JoYMM6qRt1ZcKpt1J3RZ3JPuMyXetmbHH7Mnc9C</p></div>
         </div>
