8224b35ca4b4f5d8cec9b6b1cf712c29cf8d62bb
Dale Hutchinson <daletristanhutchinson@gmail.com>
2025-01-09 12:01:52 +1100

    Add new dependencies and update layout background color for improved aesthetics

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className="min-w-screen min-h-screen text-[#000000] bg-[#e8e6e6] font-serif flex flex-col justify-start items-center [&_a]:text-[#3f81d2] [&_a]:underline list-inside [&_ul]:list-disc [&_ul]:translate-x-4 [&_h2]:text-2xl"
+        className="min-w-screen min-h-screen text-[#000000] bg-[#f5f5f5] font-serif flex flex-col justify-start items-center [&_a]:text-[#3f81d2] [&_a]:underline list-inside [&_ul]:list-disc [&_ul]:translate-x-4 [&_h2]:text-2xl"
       >
         <div className="w-11/12 md:w-2/3 p-4 flex flex-row justify-start">
           <h1 className="font-bold text-[2.5rem]">YourSite</h1>
