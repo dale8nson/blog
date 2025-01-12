9c4fac99cd70f947653194f4ca2f01adc0bcd17f
Dale Hutchinson <daletristanhutchinson@gmail.com>
2025-01-13 07:25:03 +1100

    Refactor Header and Footer components: update border thickness for improved visual consistency

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -10,13 +10,13 @@ const Footer = async ({ slug }: { slug?: string }) => {
   const footer = post?.footer || site.defaultFooter || null
 
   if (footer) return (
-    <header className="w-11/12 md:w-2/3 flex flex-row justify-start items-center my-4 border-black border-t-2 border-solid">
+    <header className="w-11/12 md:w-2/3 flex flex-row justify-start items-center my-4 border-black border-t-[1.5px] border-solid">
       <Entry node={footer as types.Block} />
     </header>
   )
 
   return (
-    <footer className="w-11/12 md:w-2/3 flex flex-row flex-grow-0 justify-start items-start my-4 border-black border-t-2 mt-6 border-solid pt-4">
+    <footer className="w-11/12 md:w-2/3 flex flex-row flex-grow-0 justify-start items-start my-4 border-black border-t-[1.5px] mt-6 border-solid pt-4">
       <p>© {new Date().getFullYear()} {title as string}. All rights reserved.</p>
     </footer>
   )
